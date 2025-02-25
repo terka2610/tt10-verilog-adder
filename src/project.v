e98873a8125aea379c91c3ea16e725c5c376ee22
terka2610 <122984973+terka2610@users.noreply.github.com>
2025-02-25 12:54:40 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,7 +17,7 @@ module tt_um_boolean (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out  = (ui_in & uio_in) | (~ui_in & uio_in);  // Implementing the Boolean logic function
   assign uio_out = 0;
   assign uio_oe  = 0;
 
@@ -25,3 +25,4 @@ module tt_um_boolean (
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
+
